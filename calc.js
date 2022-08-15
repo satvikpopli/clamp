dd60147400ec1ef8c7669007fa261dc365b92835
Satvik Popli <satvikpopli4@gmail.com>
2022-08-15 10:44:22 +0530

    Fix bugs

--- calc.js
+++ calc.js
@@ -4,16 +4,35 @@ document.getElementById("btn").addEventListener("click", function () {
     const min_val = document.getElementById('min-value').value;
     const max_val = document.getElementById('max-value').value;
     const slope = (max_val - min_val) / (max_width - min_width);
-    const k = min_val - (slope * min_width);
+    let k = min_val - (slope * min_width);
     let str;
     if (k >= 0) {
-        str = `clamp(${min_val}rem, ${(100 * slope).toFixed(3)} vw + ${k.toFixed(3)} rem, ${max_val}rem)`;
+        str = `clamp(${min_val}rem, ${(100 * slope).toFixed(3)}vw + ${k.toFixed(3)}rem, ${max_val}rem)`;
     } else {
-        str = `clamp(${min_val}rem, ${(100 * slope).toFixed(3)} vw ${k.toFixed(3)} rem, ${max_val}rem)`;
+        k -= (2 * k);
+        str = `clamp(${min_val}rem, ${(100 * slope).toFixed(3)}vw - ${k.toFixed(3)}rem, ${max_val}rem)`;
     }
-    const node = document.createElement("p");
-    const textnode = document.createTextNode(str);
-    node.appendChild(textnode);
-    console.log(node);
-    document.getElementById("formula").appendChild(node);
+    const formula = document.getElementById("formula");
+    formula.childNodes[4]?.remove();
+    formula.childNodes[3]?.remove();
+    const p = document.createElement("p");
+    const strtextnode = document.createTextNode(str);
+    const br = document.createElement("br");
+    const btn = document.createElement("button");
+    const copy = document.createTextNode("Copy");
+    const copied = document.createTextNode("Copied!");
+    p.appendChild(strtextnode);
+    formula.appendChild(p);
+    btn.classList.add("btn");
+    btn.appendChild(copy);
+    formula.appendChild(btn);
+    btn.addEventListener("click", function () {
+        navigator.clipboard.writeText(str);
+        formula.appendChild(br);
+        formula.appendChild(copied);
+        setTimeout(function () {
+            formula.removeChild(br);
+            formula.removeChild(copied);
+        }, 500);
+    });
 });
